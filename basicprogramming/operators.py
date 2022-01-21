3ff9fe6459e781a8362785c4484eacc302fa8a34
sanketnimbalkar <sanath4096@gmail.com>
2022-01-21 18:42:27 +0530

    Initial commit

---
+++ basicprogramming/operators.py
@@ -0,0 +1,31 @@
+# Task 1 add two integer and check
+
+i1 = 10
+i2 = 20
+
+res_i = i1 + i2
+
+print(res_i)
+print(type(res_i))
+
+# Task 2 add two float and check.
+
+f1 = 10.5
+f2 = 15.6
+
+res_f = f1 + f2
+
+print(res_f)
+print(type(res_f))
+
+# Task to add float and int and check.
+
+v1 = 5
+v2 = 10.0
+
+res_v = v1 + v2
+
+print(res_v)
+print(type(res_v))
+
+
---
+++ basicprogramming/variablesobjects.py
@@ -0,0 +1,13 @@
+i = 10
+
+print(type(i))
+
+j: int = 10
+
+j = 'Hello'
+
+print(j)
+
+print(type(j))
+
+print(type(j) == str)
